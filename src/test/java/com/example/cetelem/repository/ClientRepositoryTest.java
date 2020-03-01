76a88daa88ea6908365356bde2930adafe8f4019
Joao Machado <joao_m_s_m@hotmail.com>
2020-03-01 20:50:38 +0000

    add TDD to Client Repository

---
+++ src/test/java/com/example/cetelem/repository/ClientRepositoryTest.java
@@ -0,0 +1,36 @@
+package com.example.cetelem.repository;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.junit.Ignore;
+import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import com.example.cetelem.model.Client;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
+public class ClientRepositoryTest {
+
+	@Autowired
+	private ClientRepository repository;
+
+	@Test
+	public void testAllMusics() throws Exception {
+
+		List<Client> actualList = new ArrayList<Client>();
+
+		repository.findAll().forEach((c) -> actualList.add(c));
+		
+		assertFalse(actualList.isEmpty());
+		assertEquals(1, actualList.size());
+	}
+}
