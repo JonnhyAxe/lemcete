03d7efe7a311f66278d23265fee62a391366de16
Joao Machado <joao_m_s_m@hotmail.com>
2020-03-01 21:04:49 +0000

    add RepositoryRestResource Repository

--- src/main/java/com/example/cetelem/repository/ClientRepository.java
+++ src/main/java/com/example/cetelem/repository/ClientRepository.java
@@ -1,9 +1,11 @@
 package com.example.cetelem.repository;
 
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
 import com.example.cetelem.model.Client;
 
+@RepositoryRestResource(collectionResourceRel = "users", path = "users")
 public interface ClientRepository extends CrudRepository<Client, Long> {
 
 }
--- src/main/java/com/example/cetelem/repository/SallesSellRepository.java
+++ src/main/java/com/example/cetelem/repository/SallesSellRepository.java
@@ -1,9 +1,11 @@
 package com.example.cetelem.repository;
 
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
 import com.example.cetelem.model.SallesSell;
 
+@RepositoryRestResource(collectionResourceRel = "sallesSell", path = "sallesSell")
 public interface SallesSellRepository  extends CrudRepository<SallesSell, Long> {
 
 }
